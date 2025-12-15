bb5d3e84283a8448dc705f0dad275f7a136754b1
Aygox <155141069+Aygox-XIV@users.noreply.github.com>
2025-12-15 15:12:20 -0800

    Add a note about data being incomplete

--- app/menu/header.tsx
+++ app/menu/header.tsx
@@ -4,7 +4,10 @@ import type { NoProps } from '../util';
 export const Header: React.FC<NoProps> = ({}) => {
     return (
         <div className="header">
-            <NavLink to="/">TinyCollector -- a collection tracker for Tiny Shop</NavLink>
+            <div className="title">
+                <NavLink to="/">TinyCollector -- a collection tracker for Tiny Shop</NavLink>
+            </div>
+            <div className="note">(NOTE: data is still incomplete)</div>
         </div>
     );
 };
